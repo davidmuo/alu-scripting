9ae817b7e5c151dbba7c3a540c1dc6de4e71709a
davidmuo <d.muotohfra@alustudent.com>
2024-10-26 22:00:08 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+function that queries the 'Reddit API'
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'MyBot/1.0'}
+    headers = {'User-Agent': 'Reddit-hotposts-checker/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
